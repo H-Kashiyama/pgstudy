046c798743f3037798b10c91f0c771c55c68b4e5
hideto.kashiyama <grant620523@gmail.com>
2019-09-28 06:38:05 +0000

    pgstudy8

--- app/models/ctg.rb
+++ app/models/ctg.rb
@@ -1,3 +1,4 @@
 class Ctg < ApplicationRecord
     has_many :tchings
+    validates :ctg, presence: true, length: { maximum: 255 }
 end
--- app/models/tching.rb
+++ app/models/tching.rb
@@ -1,3 +1,6 @@
 class Tching < ApplicationRecord
   belongs_to :ctg
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :content, presence: true
+  validates :ctg_id, presence: true
 end
